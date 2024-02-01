f3c60654340a939087a482c3b721e6974f087e37
Balajikarthikean <krkarthic2012@gmail.com>
2024-02-01 14:23:22 +0530

    updated product page

--- src/main/java/com/Limeroad_Bug_Finders/pom/ProductPage.java
+++ src/main/java/com/Limeroad_Bug_Finders/pom/ProductPage.java
@@ -2,6 +2,14 @@ package com.Limeroad_Bug_Finders.pom;
 
 public interface ProductPage {
 	
-	String mom_and_baby_tab_xpath = "//a[text() = 'mom & baby']";
+	String select_gender_id = "shopMen";
+	String men_click_Xpath ="//div[contains(text(),'Men')]";
+	String mouse_hover_Xpath ="(//div[@class='fs13 fwB taC ttU vM dIb ls1 cuP'])[2]";
+	String Select_TShirt = "(//a[text()='t-shirts'])[1]";
+	String get_product_id = "659fc10c13cb38081ce67dca";
+	String  get_product_item_id ="19114456";
+	String get_product_size_id ="size_30993352";
+	
+	
 
 }
--- src/main/java/com/Limeroad_Bug_Finders/pom/ProductPageImpl.java
+++ src/main/java/com/Limeroad_Bug_Finders/pom/ProductPageImpl.java
@@ -7,11 +7,54 @@ import org.openqa.selenium.support.PageFactory;
 import com.Limeroad_Bug_Finders.base.BaseClass;
 
 public class ProductPageImpl extends BaseClass implements ProductPage{
-
+	
 	public ProductPageImpl() {
 		PageFactory.initElements(driver, this);
 	}
+
+	@FindBy(id = select_gender_id)
+	private WebElement men;
+	public WebElement getElement() {
+		return men;
+	}
 	
-	@FindBy(xpath = mom_and_baby_tab_xpath)
-	private WebElement momAndBaby_tab;
+	@FindBy(xpath =men_click_Xpath )
+	private WebElement menClick;
+	public WebElement getMenClick() {
+		return menClick;
+	}
+
+	
+	@FindBy(xpath=mouse_hover_Xpath)
+	private WebElement overmenoption;
+	public WebElement getOvermenoption() {
+		return overmenoption;
+	}
+	
+	@FindBy(xpath = Select_TShirt)
+	private WebElement TShirts;
+    public WebElement getTShirts() {
+		return TShirts;
+	}
+    
+    @FindBy(id =get_product_id)
+    private WebElement product;
+    public WebElement getProduct() {
+		return product;
+	}
+    
+    @FindBy(id=get_product_item_id)
+    private WebElement productItem;
+    public WebElement getProductItem() {
+		return productItem;
+	}
+	
+    @FindBy(id=get_product_size_id)
+    private WebElement productSize;
+	
+	public WebElement getProductSize() {
+		return productSize;
+	}
+
+
 }
