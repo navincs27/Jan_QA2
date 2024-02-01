75a37c959d6444ba9d50efd94a96f7671b4d1d04
HemalathaRanganathan <hemapriya668@gmail.com>
2024-02-01 13:03:51 +0530

    Elements added in product page

--- src/main/java/com/Limeroad_Bug_Finders/pom/ProductPage.java
+++ src/main/java/com/Limeroad_Bug_Finders/pom/ProductPage.java
@@ -3,5 +3,6 @@ package com.Limeroad_Bug_Finders.pom;
 public interface ProductPage {
 	
 	String mom_and_baby_tab_xpath = "//a[text() = 'mom & baby']";
+	
 
 }
--- src/main/java/com/Limeroad_Bug_Finders/pom/ProductPageImpl.java
+++ src/main/java/com/Limeroad_Bug_Finders/pom/ProductPageImpl.java
@@ -14,4 +14,50 @@ public class ProductPageImpl extends BaseClass implements ProductPage{
 	
 	@FindBy(xpath = mom_and_baby_tab_xpath)
 	private WebElement momAndBaby_tab;
+	@FindBy(xpath = "//p[text()='Women']")
+    private WebElement element1;
+    
+	@FindBy(xpath ="//a[@id='women_category']")
+	private WebElement element2;
+	
+	@FindBy(xpath = "//a[text () = 'Palazzos & Skirts']")
+	private WebElement element3;
+	
+	@FindBy(xpath = "//img[@id ='18016717']")
+	private WebElement element4;
+	
+	@FindBy(xpath = "//span[@id = 'size_28306531']")
+	private WebElement element5;
+	
+	@FindBy(xpath ="//div[text() = 'ADD TO CART']")
+	private WebElement element6;
+	
+		public WebElement getElement1() {
+		return element1;
+		
+	}
+	public WebElement getElement2() {
+		return element2;
+		
+	}
+	public WebElement getElement3() {
+		return element3;
+		
+	}
+	
+	public WebElement getElement4() {
+			return element4;
+			
+		}	
+public WebElement getElement5() {
+		return element5;
+		
+	}
+public WebElement getElement6() {
+	return element6;
+	
+}
+	}
+	
+
 }
