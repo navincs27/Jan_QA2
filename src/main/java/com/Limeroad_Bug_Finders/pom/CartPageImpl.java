37dfe7f9fc4979a677bd2b16936b0282876a2c88
MuthuKumar271 <muthuKumarrajendran271@gmail.com>
2024-02-02 10:45:29 +0530

    new methods

--- src/main/java/com/Limeroad_Bug_Finders/pom/CartPageImpl.java
+++ src/main/java/com/Limeroad_Bug_Finders/pom/CartPageImpl.java
@@ -5,11 +5,16 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.Limeroad_Bug_Finders.base.BaseClass;
+import com.github.dockerjava.transport.DockerHttpClient.Request.Method;
 
 public class CartPageImpl extends BaseClass implements CartPage {
 	
 	public CartPageImpl() {
 		PageFactory.initElements(driver, this);
+	}
+	
+	private void name() {
+		
 	}
 	
 	@FindBy(xpath = mom_and_baby_tab_xpath)
